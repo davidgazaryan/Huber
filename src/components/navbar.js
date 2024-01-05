0308709a2151db32f8ea3cc800447ede04b5e21a
David Gazaryan <126544535+davidgazaryan@users.noreply.github.com>
2024-01-05 00:21:02 -0500

    edit navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 function NavBar(){
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [activeNavbar, setActiveNavbar] = useState(false);
+
+    // use location hook to expand, navbar
+
+
     return(
         <div className='navbar'>
             <div className='links'>
