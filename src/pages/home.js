635e97fba94134b0b957ac89045b87cf31eeabfe
David Gazaryan <126544535+davidgazaryan@users.noreply.github.com>
2024-01-14 17:40:32 -0500

    added assets

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,20 @@
-import React from "react"
+import React from "react";
+import photo1 from '../photos/photo1.jpg';
+import '../styles/home.css'
+import { Link } from "react-router-dom";
 
 export const Home = () => {
     return(
-        <div>Hello world</div>
+        <div className="home">
+            <div className="homecontainer">
+                <div className="containerleft">
+                    <button>
+                        <Link to='/services'>Services</Link>
+                    </button>
+                </div>
+                <img src={photo1}></img>
+            </div>
+        </div>
+        
     )
 }
