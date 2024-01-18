e307e9203f13314e48d45b0864dc4c23e6dfd961
David Gazaryan <126544535+davidgazaryan@users.noreply.github.com>
2024-01-18 16:54:08 -0500

    fixed navbar styling

--- src/pages/miscellaneous.js
+++ src/pages/miscellaneous.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import '../styles/miscellaneous.css';
 
 export const Miscellaneous = () => {
+
+    const [width,setWidth] = useState(window.innerWidth);
+
+    useEffect (() => {
+        const WidthAdjuster = () =>{
+            setWidth(window.innerWidth)
+        };
+        
+        window.addEventListener('resize',WidthAdjuster);
+
+        return () => window.removeEventListener('resize',WidthAdjuster);
+
+    },[])
+
     return (
         <div className="main">
             <div className="notary">
@@ -18,6 +33,16 @@ export const Miscellaneous = () => {
                     Whether you're navigating business negotiations, legal proceedings, healthcare appointments, or any other scenario, I provide clear 
                     and accurate interpretation to ensure effective communication. 
                 </p>
+            </div>
+            <div className="contactcontainer">
+                <div>
+                    <h3>Contact Me {width > 840 ? <span aria-label="" role='img'>&#x1F449;</span> : <span role='img' aria-label="">&#x1F447;</span>}</h3>
+                </div>
+                <div>
+                    <a className="linkedina" href="https://www.linkedin.com/in/david-gazaryan-a7591217b" target="_blank" rel="noopener noreferrer">LinkedIn</a>    
+                    <a href="mailto:holyor87@gmail.com" target="_blank" rel="noopener noreferrer">holyor87@gmail.com</a>
+                </div>
+                
             </div>
         </div>
     )
