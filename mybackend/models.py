84ff1456c8be142aaab2409c72078172f6eb2fd7
David Gazaryan <126544535+davidgazaryan@users.noreply.github.com>
2024-02-12 22:21:16 -0500

    added validators

--- mybackend/models.py
+++ mybackend/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here.
 
@@ -7,5 +8,6 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length = 100)
     body = models.TextField()
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MaxValueValidator(5,message="Choose a number from 1-5"),MinValueValidator(1,message='Choose a number from 1-5')])
     date_created = models.DateTimeField(auto_now_add = True)
+    
