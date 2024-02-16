073bfe89d758bcbfc94682ca4c93fc483b516f5a
David Gazaryan <126544535+davidgazaryan@users.noreply.github.com>
2024-02-16 00:02:21 -0500

    login/signup

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import {FaUser,FaLock} from "react-icons/fa"
+import '../styles/login.css';
+import { Link } from "react-router-dom";
 
 export const Login = () => {
     return(
-        <div>
-            Login page
+        <div className="main-login">
+            <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="email" placeholder="Email.." required/>
+                    <FaUser className="icon"/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password.." required/>
+                    <FaLock className="icon"/>
+                </div>
+                <button type="submit">Login</button>
+                <div className="register-link">
+                    <p>Don't have an account? <Link to='/signup'>Register</Link></p>
+                </div>
+            </form>
         </div>
     )
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import '../styles/signup.css';
+import { FaUser,FaLock } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const SignUp = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+
     return(
-        <div>
-            Sign Up page
+        <div className="main-login">
+            <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="email" placeholder="Email.." required/>
+                    <FaUser className="icon"/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password.." required/>
+                    <FaLock className="icon"/>
+                </div>
+                <button type="submit">Login</button>
+                <div className="register-link">
+                    <p>Already have an account? <Link to="/login">Sign In</Link></p>
+                </div>
+            </form>
         </div>
     )
 }
